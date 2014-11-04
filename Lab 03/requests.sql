027754009b26422c483e20947acccc1687fd6028
Sacha Bron <sacha.bron@copperflake.com>
2014-11-04 11:55:45 +0100

    requets 14-17

--- Lab 03/requests.sql
+++ Lab 03/requests.sql
@@ -52,3 +52,59 @@ WHERE f.length = (
 		MIN(f.length)
 	FROM film f
 )
+
+-- 14) Lister les acteurs (actor_id, nombre_films) qui ont joué dans plus de 35 films, sans utiliser de sous-requêtes.
+
+SELECT
+	a.actor_id AS actor_id,
+	COUNT(fa.film_id) AS nombre_films
+FROM actor a
+JOIN film_actor fa ON fa.actor_id = a.actor_id 
+GROUP BY a.actor_id
+HAVING nombre_films >= 35
+
+-- 15) Lister les films (id, titre) dont l’identifiant est inférieur à 100, ordonnés par id dans lesquels joue au moins un acteur qui a joué dans plus de 35 films. Utiliser le mot clé IN.
+
+SELECT
+	f.film_id AS id,
+	f.title AS titre
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE f.film_id <= 100
+	AND a.actor_id IN (
+		SELECT
+			a.actor_id AS actor_id
+		FROM actor a
+		JOIN film_actor fa ON fa.actor_id = a.actor_id 
+		GROUP BY a.actor_id
+		HAVING COUNT(fa.film_id) >= 35
+	)
+ORDER BY f.film_id
+
+-- 16) Même question, mais sans utiliser le mot clé IN. Indication : une sous-requête peut être utilisée comme un table, et donc être jointe.
+-- Quelle requête est la plus rapide (celle-ci ou la précédente)? A votre avis, pourquoi?
+
+SELECT
+	f.film_id AS id,
+	f.title AS titre
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+JOIN (
+		SELECT
+			a.actor_id AS actor_id
+		FROM actor a
+		JOIN film_actor fa ON fa.actor_id = a.actor_id 
+		GROUP BY a.actor_id
+		HAVING COUNT(fa.film_id) >= 35
+	) a ON a.actor_id = fa.actor_id
+WHERE f.film_id <= 100
+ORDER BY f.film_id
+
+-- La requête la plus rapide sera celle-ci (la n°16). En effet, le DBMS n'aura pas besoin de charger 2 fois la table `actor` et de chercher si chaque actor est présent dans la table `actor` réduite. Toutes ces comparaisons permettent de gagner du temps.
+
+-- 17) Un fou décide de regarder l'ensemble des films qui sont présents dans la base de données. Etablir une requête qui donne le nombre de jours (jours) qu'il devra y consacrer, s'il dispose de 16h par jour.
+
+SELECT
+	SUM(f.length)/60/16 AS jours
+FROM film f
